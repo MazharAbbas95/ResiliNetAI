bc06a51d080c4f10ea6eaed80222ce6113d62be4
MazharAbbas95 <mazharabbasawan95gmail.com>
2026-05-20 05:48:05 -0700

    Fix: Add easignore and block backend from native compilation loop

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,9 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+config.resolver.blockList = [
+  /backend\/.*/,
+  /node_modules\/.*\/node_modules\/fbjs.*/
+];
+
 module.exports = config;
